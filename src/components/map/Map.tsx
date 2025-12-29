d97c5b47ea17d25bc959d581d79c7ec21c697d5c
Grandschtroumpf <francois.guezengar@hotmail.fr>
2025-12-29 18:10:58 +0100

    improve homepage

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,51 +1,31 @@
 import { component$, useStyles$} from "@qwik.dev/core";
-import { Logo } from "../logo/Logo";
 import style from './Map.css?inline';
 
-export default component$(() => {
+export const Map = component$(() => {
   useStyles$(style);
 
 
   return (
     <section id="map">
-      <article id="welcome" class="is-visible">
-        <div class="presentation">
-          <Logo class="place-self-center logo animated" />
-          <hgroup>
-            <p>Erwan Richard</p>
-            <h1>Le P'tit Menuisier</h1>
-          </hgroup>
-          <div class="grid grid-cols-2 place-items-center">
-            <p>Intérieur</p>
-            <p>Extérieur</p>
-          </div>
-          <a class="btn-fill" href="/simulateur">
-            Simuler un Devis
-          </a>
-        </div>
-      </article>
-      <article class="imgs">
-        {/* eslint-disable-next-line qwik/jsx-img */}
-        <img src="/imgs/map.svg" width="1920" height="1080" />
-        <div class="img-filter"></div>
-        <div class="location" style="--w:0.5;--h:0.5">
-          <div class="circle second"></div>
-          <div class="circle"></div>
-          <p>
-            {Array.from('Redon').map((letter, i) => <span key={i} style={{'--index': i}}>{letter}</span>)}
-          </p>
-        </div>
-        <div class="location" style="--w:0.61;--h:0.20;--delay:300ms">
-          <p>
-            {Array.from('Rennes').map((letter, i) => <span key={i} style={{'--index': i}}>{letter}</span>)}
-          </p>
-        </div>
-        <div class="location" style="--w:0.64;--h:0.78;--delay:400ms">
-          <p>
-            {Array.from('Nantes').map((letter, i) => <span key={i} style={{'--index': i}}>{letter}</span>)}
-          </p>
-        </div>
-      </article>
+      {/* eslint-disable-next-line qwik/jsx-img */}
+      <img src="/imgs/map.svg" width="1920" height="1080" />
+      <div class="location" style="--w:0.5;--h:0.5">
+        <div class="circle second"></div>
+        <div class="circle"></div>
+        <p>
+          {Array.from('Redon').map((letter, i) => <span key={i} style={{'--index': i}}>{letter}</span>)}
+        </p>
+      </div>
+      <div class="location" style="--w:0.61;--h:0.20;--delay:300ms">
+        <p>
+          {Array.from('Rennes').map((letter, i) => <span key={i} style={{'--index': i}}>{letter}</span>)}
+        </p>
+      </div>
+      <div class="location" style="--w:0.64;--h:0.78;--delay:400ms">
+        <p>
+          {Array.from('Nantes').map((letter, i) => <span key={i} style={{'--index': i}}>{letter}</span>)}
+        </p>
+      </div>
     </section>
   )
 })
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,13 @@
-import { component$, useOn, $ } from "@qwik.dev/core";
+import { component$, useOn, $, useStyles$ } from "@qwik.dev/core";
 import { DocumentHead } from "@qwik.dev/router";
 import { Testimonies } from "~/components/testimonies/testimonies";
-import Map from "~/components/map/Map";
+import { Map } from "~/components/map/Map";
+import { Welcome } from "~/components/welcome/welcome";
+import { Logo } from "~/components/logo/Logo";
+import style from './index.css?inline';
 
 export default component$(() => {
-  
+  useStyles$(style);
   useOn('qvisible', $(() => {
     const observer = new IntersectionObserver((entries) => {
       for (const entry of entries) {
@@ -21,10 +24,14 @@ export default component$(() => {
   }));
 
   return (
-    <main>
-      <Map />
-      <Testimonies />
-    </main>
+    <>
+      <Logo width="300" height="300" class="logo" />
+      <main>
+        <Welcome />
+        <Map />
+        <Testimonies />
+      </main>
+    </>
   )
 })
 

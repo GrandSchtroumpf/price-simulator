0230e9b48f8f357a1a87ca6917c01b6bf30f52da
NICOLAS MICHEL <nicolas_m_44@hotmail.fr>
2026-01-08 15:33:51 +0100

    assets: modification of placeholders

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -7,15 +7,15 @@ export default component$(() => {
 		<section class="gallery">
 			<figure style="--index:1">
 				<figcaption>Nantes</figcaption>
-				<img loading="lazy" src="/imgs/home/stroumpf.webp" alt="Sculpture de Stroumpf" height="300" />
+				<img loading="lazy" src="/imgs/home/carpentry.webp" alt="Ouvrage de menuiserie" height="300" />
 			</figure>
 			<figure style="--index:2">
-				<figcaption>La Trinité sur Mer</figcaption>
-				<img loading="lazy" src="/imgs/home/stroumpf.webp" alt="Sculpture de Stroumpf" height="300" />
+				<figcaption>Redon</figcaption>
+				<img loading="lazy" src="/imgs/home/carpentry.webp" alt="Ouvrage de menuiserie" height="300" />
 			</figure>
 			<figure style="--index:3">
 				<figcaption>Saint Nazaire</figcaption>
-				<img loading="lazy" src="/imgs/home/stroumpf.webp" alt="Sculpture de Stroumpf" height="300" />
+				<img loading="lazy" src="/imgs/home/carpentry.webp" alt="Ouvrage de menuiserie" height="300" />
 			</figure>
 		</section>
 	)

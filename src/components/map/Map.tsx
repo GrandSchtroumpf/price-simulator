7ee64bb3bf5639571c8fa760d6e5a81ab93f9e2f
NICOLAS MICHEL <nicolas_m_44@hotmail.fr>
2026-01-14 15:55:31 +0100

    aria-labelledby added

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -4,7 +4,7 @@ import style from './Map.css?inline';
 export const Map = component$(() => {
   useStyles$(style);
   return (
-    <section id="map">
+    <section id="map" aria-labelledby="map-legend">
       {/* eslint-disable-next-line qwik/jsx-img */}
       <img fetchPriority="high" src="/imgs/map.svg" width="1920" height="1080" alt="Carte représentant la Loire-Atlantique, le Morbihan et l'Ille-et-Vilaine" aria-description="Erwan Richard, menuisier professionnel, travaille à Redon et peut se déplacer jusqu'à 50 kilomètres autour de cette ville" />
       <div class="location" style="--w:0.5;--h:0.5">

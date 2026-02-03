77998a987cb50359bcb516bdb5fa33f2aa75581a
jeremy <jeremy.marzuolo@gmail.com>
2026-02-03 10:36:35 +0100

    switch back to href

--- src/routes/simulateur/cart/index.tsx
+++ src/routes/simulateur/cart/index.tsx
@@ -25,12 +25,6 @@ export default component$(() => {
     }
   });
 
-  const editItem = $((dynamicFormKey: string, i: number) => {
-    if (!dynamicFormKey) return;
-    navigate(`/simulateur/${dynamicFormKey}?index=${i}`);
-  }
-  );
-
   const onSubmit = $(async (form: HTMLFormElement) => {
     const isValid = form.checkValidity();
     if (!isValid) return;
@@ -61,12 +55,11 @@ export default component$(() => {
                   <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
                 </svg>
                 <h3>{dynamicForm.label}</h3>
-                <button onClick$={() => editItem(dynamicFormKey, i)}>
-                  test
-                </button>
-                <svg aria-hidden height="24px" width="24px" viewBox="0 -960 960 960" fill="currentColor">
-                  <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" />
-                </svg>
+                <a href={`/simulateur/${dynamicFormKey}?index=${i}`} aria-label="modifier">
+                  <svg aria-hidden height="24px" width="24px" viewBox="0 -960 960 960" fill="currentColor">
+                    <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" />
+                  </svg>
+                </a>
                 <button onClick$={() => cart.splice(i, 1)} aria-label="supprimer">
                   <svg aria-hidden height="24px" width="24px" viewBox="0 -960 960 960" fill="currentColor">
                     <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />

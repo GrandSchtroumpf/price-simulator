f039d8e102c18056950a4f71671a9f3dec2ac217
jeremy <jeremy.marzuolo@gmail.com>
2026-02-03 15:22:22 +0100

    remove door form

--- src/routes/simulateur/forms/index.ts
+++ src/routes/simulateur/forms/index.ts
@@ -1,13 +1,11 @@
 import { interior } from "./interior";
 import { deck } from "./deck";
-import { door } from "./door";
 import { stairs } from "./stairs";
 import { floor } from "./floor";
 
 export const dynamicFormRecord = {
   interior,
   deck,
-  door,
   stairs,
   floor
 }

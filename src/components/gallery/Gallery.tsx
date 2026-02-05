f5ea5e1a67135d573df1e2599a03968d5700a2eb
Grandschtroumpf <francois.guezengar@hotmail.fr>
2026-02-05 12:27:34 +0100

    improve image position on gallery

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -7,6 +7,28 @@ import Img5 from '~/media/gallery/5.webp?jsx';
 import style from './Gallery.css?inline';
 import { SplittedText } from "../SplittedText";
 
+const articles = [{
+	label: 'Terrasse et garde corps',
+	img: <Img1 alt="Ouvrage de menuiserie" height="300" />,
+	text: "Menuisier passionné, je conçois et réalise des terrasses en bois sur mesure, adaptées à votre espace et à vos usages. J'y associe des garde-corps sécurisés et esthétiques, pensés pour durer dans le temps, tout en valorisant l'architecture de votre extérieur."
+}, {
+	label: 'Soutènement bois',
+	img: <Img2 alt="Ouvrage de menuiserie" height="300" />,
+	text: "Je réalise des ouvrages de soutènement en bois et des structures de charpente robustes, adaptés aux contraintes du terrain. Chaque projet est conçu pour garantir stabilité, longévité et intégration naturelle, tout en respectant les règles de construction et le savoir-faire artisanal."
+}, {
+	label: 'Brise vue',
+	img: <Img3 alt="Ouvrage de menuiserie" height="300" />,
+	text: "Je crée des brise-vue en bois sur mesure, du muret jusqu'au plafond, en prenant en compte des matériaux adaptés à vos budgets. Chaque réalisation s'adapte à votre espace, joue avec les lignes et les essences, et apporte une touche chaleureuse et contemporaine à votre habitat."
+}, {
+	label: 'Terrasse et bardage',
+	img: <Img4 alt="Ouvrage de menuiserie" height="300" />,
+	text: "Je conçois des terrasses et bardages en bois qui structurent et embellissent votre maison. Ces réalisations associent esthétique, protection et durabilité, tout en mettant en valeur les volumes existants et le caractère naturel du bois, travaillé avec précision et exigence."
+}, {
+	label: 'Portail principal',
+	img: <Img5 alt="Ouvrage de menuiserie" height="300" />,
+	text: "Je fabrique des portails principaux en bois à coupe naturelle, alliant caractère authentique et robustesse. Chaque pièce est unique, respectant le veinage et la matière, pour offrir une entrée forte en identité, durable et parfaitement intégrée à son environnement."
+}]
+
 export default component$(() => {
 	useStyles$(style);
 	return (
@@ -15,41 +37,17 @@ export default component$(() => {
 				<div class="line"></div>
 				<div class="diamonds"></div>
 			</div>
-			<div class="slot" style="--index:1">
-				<figure>
-					<figcaption>Terrasse et garde corps</figcaption>
-					<Img1 alt="Ouvrage de menuiserie" height="300" />
-				</figure>
-				<p><SplittedText mode="word" text="Menuisier passionné, je conçois et réalise des terrasses en bois sur mesure, adaptées à votre espace et à vos usages. J'y associe des garde-corps sécurisés et esthétiques, pensés pour durer dans le temps, tout en valorisant l'architecture de votre extérieur." /></p>
-			</div>
-			<div class="slot" style="--index:2">
-				<p>Je réalise des ouvrages de soutènement en bois et des structures de charpente robustes, adaptés aux contraintes du terrain. Chaque projet est conçu pour garantir stabilité, longévité et intégration naturelle, tout en respectant les règles de construction et le savoir-faire artisanal.</p>
-				<figure>
-					<figcaption>Soutènement bois</figcaption>
-					<Img2 alt="Ouvrage de menuiserie" height="300" />
-				</figure>
-			</div>
-			<div class="slot" style="--index:3">
-				<figure>
-					<figcaption>Brise vue</figcaption>
-					<Img3 alt="Ouvrage de menuiserie" height="300" />
-				</figure>
-				<p>Je crée des brise-vue en bois sur mesure, du muret jusqu'au plafond, en prenant en compte des matériaux adaptés à vos budgets. Chaque réalisation s'adapte à votre espace, joue avec les lignes et les essences, et apporte une touche chaleureuse et contemporaine à votre habitat.</p>
-			</div>
-			<div class="slot" style="--index:4">
-				<p>Je conçois des terrasses et bardages en bois qui structurent et embellissent votre maison. Ces réalisations associent esthétique, protection et durabilité, tout en mettant en valeur les volumes existants et le caractère naturel du bois, travaillé avec précision et exigence.</p>
-				<figure>
-					<figcaption>Terrasse et bardage</figcaption>
-					<Img4 alt="Ouvrage de menuiserie" height="300" />
-				</figure>
-			</div>
-			<div class="slot" style="--index:5">
-				<figure>
-					<figcaption>Portail principal</figcaption>
-					<Img5 alt="Ouvrage de menuiserie" height="300" />
-				</figure>
-				<p>Je fabrique des portails principaux en bois à coupe naturelle, alliant caractère authentique et robustesse. Chaque pièce est unique, respectant le veinage et la matière, pour offrir une entrée forte en identité, durable et parfaitement intégrée à son environnement.</p>
-			</div>
+			{articles.map((article, i) => (
+				<article key={i} class="slot" style={{'--index': i}}>
+					<figure>
+						<figcaption>{article.label}</figcaption>
+						{article.img}
+					</figure>
+					<p>
+						<SplittedText mode="word" text={article.text} />
+					</p>
+				</article>
+			))}
 		</section>
 	)
 });
